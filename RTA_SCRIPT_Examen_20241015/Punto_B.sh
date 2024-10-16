28386b472d2ba0c27e00de7e1114e401d2c857cb
briamdotpy <ballesterosbrian03@gmail.com>
2024-10-16 01:22:23 +0000

    fix: Modificando el archivo, agregando los comandos que utilice anteriormente.

--- RTA_SCRIPT_Examen_20241015/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_B.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#Los comandos que utilice para realizar las particiones y el formateo fueron: "sudo fdisk /dev/, sudo mkfs.ext4 /dev/."
+
 # Resolviendo el punto B, Haciendo un for para poder montar las particiones que realicé con anterioridad.
 
 #
@@ -12,3 +15,5 @@ for i in {1..9}; do
     echo "/dev/sdc${i} /Examenes-UTN/parcial_$i ext4 defaults 0 0" | sudo tee -a /etc/fstab
 done
 echo "/dev/sdc10 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+
+# Intenté realizar el montaje con un for pero al parecer no lo logré." 
